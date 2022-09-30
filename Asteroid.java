e2d3be8ba013022a98666dee0f5ab569e7369d49
michelle422 <32956971+michelle422@users.noreply.github.com>
2022-09-30 10:07:02 +0200

    Cleaned the code.
    Changed data type of date from LocalDate to String

---
+++ Asteroid.java
@@ -0,0 +1,62 @@
+package com.michelle.nasa.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class Asteroid {
+	
+	private String id;
+	private String name;
+	private BigDecimal estimatedDiameter;
+	private String closeApproachDate;
+	private double missDistance;
+	
+	public Asteroid() {
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getEstimatedDiameter() {
+		return estimatedDiameter;
+	}
+
+	public void setEstimatedDiameter(BigDecimal estimatedDiameter) {
+		this.estimatedDiameter = estimatedDiameter;
+	}
+
+	public String getCloseApproachDate() {
+		return closeApproachDate;
+	}
+
+	public void setCloseApproachDate(String closeApproachDate) {
+		this.closeApproachDate = closeApproachDate;
+	}
+
+	public double getMissDistance() {
+		return missDistance;
+	}
+
+	public void setMissDistance(double missDistance) {
+		this.missDistance = missDistance;
+	}
+	
+
+}
